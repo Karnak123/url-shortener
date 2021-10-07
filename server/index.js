f358a91ffce7ebc9383992871064c1be55c7796e
Sagnik Mazumder <sagnikmazumdar37@gmail.com>
2021-10-07 11:55:28 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express'); 								// https://www.npmjs.com/package/express
 const log4js = require('log4js');                                   // https://www.npmjs.com/package/log4js
-const session = require('cookie-session');							// https://www.npmjs.com/package/cookie-session
+const session = require('express-session');							// https://www.npmjs.com/package/express-session
 const passport = require('passport');								// https://www.npmjs.com/package/passport
 const WebAppStrategy = require('ibmcloud-appid').WebAppStrategy;	// https://www.npmjs.com/package/ibmcloud-appid
 const mongoose = require('mongoose');
